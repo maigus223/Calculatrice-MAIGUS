f457474eef32fe173624c59074554474476076cc
maigus223 <158322662+maigus223@users.noreply.github.com>
2026-07-17 12:25:50 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dongoj-calc-v2';
+const CACHE_NAME = 'dongoj-calc-v3';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
