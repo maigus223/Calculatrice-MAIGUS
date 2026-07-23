8f4e8f6e8c1c554bbaaa7dc01970957f3f2414ef
maigus223 <158322662+maigus223@users.noreply.github.com>
2026-07-23 11:38:37 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ayneha-calc-v26';
+const CACHE_NAME = 'ayneha-calc-v27';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
