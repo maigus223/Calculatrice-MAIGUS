4c6a3ad789a6ffd08106d4f7273619d6b0107740
maigus223 <158322662+maigus223@users.noreply.github.com>
2026-07-23 11:16:44 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maigus-calc-v21';
+const CACHE_NAME = 'maigus-calc-v22';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
